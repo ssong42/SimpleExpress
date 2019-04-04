25816dd18f9b971fdd572d9eff8e545f7e32c592
Steven Song <ssong@e1z2r5p34.42.us.org>
2019-04-03 20:29:55 -0700

    vibration

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express()
+var api = require('termux-api').default;
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    api.createCommand()
+        .vibrate()
+        .setDuration(1000)
+        .build()
+        .run();
+    res.send('Hello World!')
+} )
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
